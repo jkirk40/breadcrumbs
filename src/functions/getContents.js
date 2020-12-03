862a8f2f0940b9e060b79d53ffbfa3f2c69c0fa6
Jamie <kirkwood.jamie@gmail.com>
2020-12-03 17:24:58 -0500

    data structure modified

--- src/functions/getContents.js
+++ src/functions/getContents.js
@@ -1,5 +1,42 @@
 import data from "../data.txt";
 
 export default function getContent () {
-    return 'test'
+
+    let root = {
+        type: "dir",
+        children: {
+            home: {
+                type: "dir",
+                    children: {
+                        myname: {
+                        type: "dir",
+                            children: {
+                            "filea.txt": {
+                                type: "file",
+                            },
+                            "fileb.txt": {
+                                type: "file",
+                            },
+                            "projects": {
+                                type: "dir",
+                                children: {
+                                    mysupersecretproject: {
+                                        type: "dir",
+                                        children: {
+                                            mysupersecretfile: {
+                                                type: "file",
+                                            },
+                                        },
+                                    }
+                                },
+                            },
+                        }
+                    },
+                },
+            }
+        },
+    };
+
+    console.log(root)
+    return 'test';
 }
