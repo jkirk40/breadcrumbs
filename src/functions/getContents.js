650546dc992805ec6db04985b3de732ca8a4b3b7
Jamie <kirkwood.jamie@gmail.com>
2020-12-03 18:26:55 -0500

    data structure modified again

--- src/functions/getContents.js
+++ src/functions/getContents.js
@@ -1,41 +1,39 @@
-import data from "../data.txt";
-
-export default function getContent () {
-
-    let root = {
-        type: "dir",
-        children: {
-            home: {
-                type: "dir",
-                    children: {
-                        myname: {
-                        type: "dir",
+const root = {
+    type: "dir",
+    children: {
+        home: {
+            type: "dir",
+                children: {
+                    myname: {
+                    type: "dir",
+                        children: {
+                        "filea.txt": {
+                            type: "file",
+                        },
+                        "fileb.txt": {
+                            type: "file",
+                        },
+                        "projects": {
+                            type: "dir",
                             children: {
-                            "filea.txt": {
-                                type: "file",
-                            },
-                            "fileb.txt": {
-                                type: "file",
-                            },
-                            "projects": {
-                                type: "dir",
-                                children: {
-                                    mysupersecretproject: {
-                                        type: "dir",
-                                        children: {
-                                            mysupersecretfile: {
-                                                type: "file",
-                                            },
+                                mysupersecretproject: {
+                                    type: "dir",
+                                    children: {
+                                        mysupersecretfile: {
+                                            type: "file",
                                         },
-                                    }
-                                },
+                                    },
+                                }
                             },
-                        }
-                    },
+                        },
+                    }
                 },
-            }
-        },
-    };
+            },
+        }
+    },
+};
+
+export default function getContent (path) {
 
     console.log(root)
     return 'test';
