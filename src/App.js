259d3e3e5728db66bc942a3f8936ad4c3d015629
Jamie <kirkwood.jamie@gmail.com>
2020-12-03 18:27:07 -0500

    app made into class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
+
 import getContent from "./functions/getContents";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      {getContent()}
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    
+    this.state = {
+      path: "/root"
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {getContent(this.state.path)}
+      </div>
+    );
+  }
 }
 
 export default App;
