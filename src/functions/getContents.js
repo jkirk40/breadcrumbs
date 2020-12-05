bc4597d0c58bbdb9b96fee558fceaa413584c6aa
Jamie <kirkwood.jamie@gmail.com>
2020-12-05 15:42:12 -0500

    improved handling of files in getContent

--- src/functions/getContents.js
+++ src/functions/getContents.js
@@ -38,7 +38,9 @@ export default async function getContent (path) {
     const traverse = (str, obj) => {
         let pathArray = str.split('/');
         if (pathArray.length === 1) {
+
             const objectFilter = (targetType) => {
+                if (obj.type === "file") {return null};
                 return Object.keys(obj.children).filter((key) => obj.children[key].type === targetType);
             }
 
