b1c869b67493df05a186771d0b7b6b077207258d
Jamie <kirkwood.jamie@gmail.com>
2020-12-05 16:14:26 -0500

    breadcrumb made recursive

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,16 +1,33 @@
 export default function Breadcrumb(props) {
     const pathArray = props.path.split('/')
+    const current = pathArray[0];
 
     const handleClick = (link) => {
-        console.log(props)
+        console.log(props.obj)
+        console.log(props.path)
         console.log(link)
+        let arr = pathArray;
+    }
+
+    const recursiveBreadcrumb = () => {
+        if (pathArray.length === 1) {
+            return '';
+        } else {
+            const newPath = pathArray.slice(1).join('/');
+            return (
+                <Breadcrumb
+                    obj={props.obj}
+                    path={newPath}
+                    updatePath={props.updatePath}
+                />
+            )
+        }
     }
 
     return (
         <div>
-            {pathArray.map((link, index) => 
-                <button onClick={() => handleClick(link)} key={index}>{link}</button>
-            )}
+            <button onClick={() => handleClick(current)}>{current}</button>
+            {recursiveBreadcrumb()}
         </div>
     )
 }
