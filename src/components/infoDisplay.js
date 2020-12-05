79376ada9e697a67fc2d7289ab3d751b9a7bcde8
Jamie <kirkwood.jamie@gmail.com>
2020-12-05 14:39:43 -0500

    different display layouts for files and subdirectories

--- src/components/infoDisplay.js
+++ src/components/infoDisplay.js
@@ -1,10 +1,21 @@
 export default function InfoDisplay (props) {
-    return (
-        <div>
-            <p>{props.obj.name}</p>
-            <p>{props.obj.type}</p>
-            <p>{props.obj.files}</p>
-            <p>{props.obj.subdirectories}</p>
-        </div>
-    )
+    if (props.obj.type === "file") {
+        return (
+            <div>
+                <p>THIS IS FILE: {props.obj.name}</p>
+            </div>
+        )
+    } else if (props.obj.type === "dir") {
+        return (
+            <div>
+                <p>Current directory: {props.obj.name}</p>
+                {props.obj.files.map((file, index) => 
+                    <p key={index}>{file}</p>
+                )}
+                {props.obj.subdirectories.map((subdirectory, index) => 
+                    <p key={index}>{subdirectory}</p>
+                )}
+            </div>
+        )
+    }
 }
