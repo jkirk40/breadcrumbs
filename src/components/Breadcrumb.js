79fed4bbb24781b8463d4f6adf9766a389d86e8c
Jamie <kirkwood.jamie@gmail.com>
2020-12-06 11:49:48 -0500

    documentation and variable name change

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -3,13 +3,24 @@ export default function Breadcrumb(props) {
     const current = pathArray[props.depth];
 
     const handleClick = () => {
+        // To define the target link for each breadcrumb, we use its depth within the stack of links
+        // eg.
+        // root is at depth 0
+        // home is at depth 1
+        // myname is at depth 2
+        // So if the current path is root/home/myname and the user clicks on 'home'
+        // we get the new path by taking the first two items in the path array:
+        // ['root', 'home', 'myname'].slice(0,2)  -----> ['root', 'home']
         const target = (pathArray.slice(0,props.depth + 1))
+        // then join the new array and send it as an update
         const newPath = target.join('/');
         props.updatePath(newPath);
+        // This could potentially be handled in react router but i wanted to minimize installed packages
     }
 
     const recursiveBreadcrumb = () => {
         if (pathArray.length - 1 === props.depth) {
+            // Do not render another breadcrumb if the current number of breadcrumbs has reached the end of the path
             return '';
         } else {
             return (
--- src/functions/getContents.js
+++ src/functions/getContents.js
@@ -38,7 +38,10 @@ export default async function getContent (path) {
     const traverse = (str, obj) => {
         let pathArray = str.split('/');
         if (pathArray.length === 1) {
+            // When pathArray.length is 1, it means we have reached the target and should return the info
+            // for the obj passed to this level.
 
+            // This returns all the children of a certain type in a directory, or null if the current object is a file
             const objectFilter = (targetType) => {
                 if (obj.type === "file") {return null};
                 return Object.keys(obj.children).filter((key) => obj.children[key].type === targetType);
@@ -53,13 +56,17 @@ export default async function getContent (path) {
 
             return result;
         } else {
-            const newArr = pathArray.slice(1).join('/');
+            // If pathArray is not 1, we are still searching through the tree
+            // so we remove the first item in the path (root/home/myname --> home/myname)
+            // and repeat this function one level deeper in the tree.
+            const newPath = pathArray.slice(1).join('/');
 
             if (!obj.children[pathArray[1]]) {
                 throw new Error('that path does not exist');
             }
 
-            return traverse(newArr, obj.children[pathArray[1]]);
+            // As arguments we pass the new path and the correct child object (based on its name)
+            return traverse(newPath, obj.children[pathArray[1]]);
         }
     }
 
